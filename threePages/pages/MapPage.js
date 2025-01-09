cf9635e57b86dfcebd83ced73085fb835f1b8d66
Jun Park <90596660+CA-JunPark@users.noreply.github.com>
2025-01-08 22:44:45 -0800

    add markers

--- threePages/pages/MapPage.js
+++ threePages/pages/MapPage.js
@@ -4,7 +4,8 @@ import { useNavigation } from '@react-navigation/native';
 import  style  from './styles';
 import MapView, { PROVIDER_GOOGLE, Marker }from 'react-native-maps';
 import { useState, useEffect } from 'react';
-import {requestLocationPermission, useCurrentLocation} from './googleMapController';
+import {useCurrentLocation} from './googleMapController';
+import {markers} from './markers';
 
 const MapPage = () => {
   const navigation = useNavigation();
@@ -21,6 +22,8 @@ const MapPage = () => {
   };
 
   const {location, error} = useCurrentLocation(); // Call the custom hook
+  const [markers, setMarkers] = useState([]); // Initialize the markers array
+  const [markerCount, setMarkerCount] = useState(1); // Initialize the marker count
 
   if (error) {
     return (
@@ -41,12 +44,52 @@ const MapPage = () => {
   }
 
   const region = {
-    latitude: location.latitude,
-    longitude: location.longitude,
+    // my current location (gps)
+    // latitude: location.latitude,
+    // longitude: location.longitude,
+    
+    // 경주
+    latitude: 35.8561719,
+    longitude: 129.2247477,
     latitudeDelta: 0.005, // Adjust the delta for zoom level
     longitudeDelta: 0.005, // Adjust the delta for zoom level
   };
 
+  
+  const addMarker = (event) => {
+    const latitude = event.nativeEvent.coordinate.latitude;
+    const longitude = event.nativeEvent.coordinate.longitude;
+    markers.push({
+      latitude: latitude,
+      longitude: longitude,
+      key: Math.random().toString(), // Generate a random key
+      title: "New Marker",
+      description: "markerCount: " + markerCount,
+    });// Add a new marker to the array
+    console.log(markers); // Log the markers array
+    setMarkers([...markers]); // Update the markers array
+    setMarkerCount(markerCount + 1); // Update the marker count
+  };
+
+  const mapStyle = [
+    // Hides labels for all features.
+    // {
+    //   elementType: 'labels',
+    //   stylers: [{ visibility: 'off' }],
+    // },
+    // Removes points of interest (e.g., restaurants, landmarks).
+    {
+      featureType: 'poi',
+      stylers: [{ visibility: 'off' }],
+    },
+    // Removes transit-related markers.
+    // {
+    //   featureType: 'transit',
+    //   stylers: [{ visibility: 'off' }],
+    // },
+  ];
+
+  //https://github.com/react-native-maps/react-native-maps/blob/master/docs/mapview.md
   return (
     <View style={style.container}>
       <MapView
@@ -54,9 +97,21 @@ const MapPage = () => {
         provider={PROVIDER_GOOGLE}
         region={region}
         showsUserLocation={true} // Shows a blue dot for user's location
+        showsMyLocationButton={true} // Srhows a button to center the map on user's location
         loadingEnabled={true}
+        customMapStyle={mapStyle}
+        onPress={addMarker}
       >
-        <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} title="You are here" />
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+            title={marker.title}
+            description={marker.description}
+            pinColor="red"
+          />
+        ))}
+        {/* <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} title="You are here" /> */}
       </MapView>
     </View>
   );
---
+++ threePages/pages/markers.js
@@ -0,0 +1,8 @@
+export const markers =[
+    {
+        latitude:  49.100376,
+        longitude:  -122.673058,
+        title: "Willowbrook Shopping Centre",
+        description: "This is the first marker"
+    },    
+]
